cf1717d00d1ea1421ef4b6003af2ae39d74b107d
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-09-11 00:13:45 +0300

    feat: add departments controller, change worker class and migrate database

--- Warehouse/Migrations/20230910211154_Workers Names Update.Designer.cs
+++ Warehouse/Migrations/20230910211154_Workers Names Update.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warehouse.Models;
 
@@ -10,9 +11,11 @@ using Warehouse.Models;
 namespace Warehouse.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230910211154_Workers Names Update")]
+    partial class WorkersNamesUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Warehouse/Migrations/20230910211154_Workers Names Update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkersNamesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Workers",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Workers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Workers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Workers",
+                newName: "Name");
+        }
+    }
+}
